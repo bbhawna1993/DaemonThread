2825df15e9b82fc4ed6317ef0cc346d1cb7efc99
bhawna <bhawnabudhiraja1993@gmail.com>
2019-05-04 13:15:58 +0530

    push daemon thread

--- src/com/io/daemon/DemoDaemon.java
+++ src/com/io/daemon/DemoDaemon.java
@@ -5,6 +5,11 @@ public class DemoDaemon {
 	public static void main(String[] args)
 	{
 		//Check if the current thread is daemon or not.
+		/*Main thread is non daemon.We can't change the daemon nature 
+		of main thread because it gets automatically started by JVM.
+         after it is started,if we change the daemon nature then it will throw
+         IllegalThreadStateException.
+		 */
 		System.out.println(Thread.currentThread().isDaemon());
 	}
 
